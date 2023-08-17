12b25ceea699a96c24253fe0a497b4094c21c057
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-08-17 21:35:10 +0100

    added trending movies router to app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import "./index.css";
+import TrendingMovies from "./pages/TrendingMovies";
 
 function App() {
 	const router = createBrowserRouter([
 		{
 			path: "/",
 			element: <Home />,
+		},
+		{
+			path: "/trending",
+			element: <TrendingMovies />,
 		},
 	]);
 
