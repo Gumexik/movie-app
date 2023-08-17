0ec813e8946a928f992b4929619448a3c3011a37
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-08-17 21:31:38 +0100

    Config file created to hold API url and config

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3/";
+export const TMDB_FETCH_OPTIONS = {
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		Authorization:
+			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjc1N2M2MGMyYzBjYTc5N2FlYzFjMmMwM2U5ODJhMCIsInN1YiI6IjY0OTg1OWY1YmJkMGIwMDEyNTJiMzUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sD0BJJ8K27GKxVohpq_1EJMJHyhr192OYHWaORaH6zY",
+	},
+};
