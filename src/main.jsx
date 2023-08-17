2a433c96e8843c74663be77f640a99b887c399e3
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-08-17 20:46:59 +0100

    router provider moved to app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-	},
-]);
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<RouterProvider router={router} />
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className='max-w-7xl mx-auto'>
+			<h1 className='p-4 text-center text-xl font-bold'>
+				Movie Search Application
+			</h1>
+		</div>
+	);
 };
 
 export default Home;
