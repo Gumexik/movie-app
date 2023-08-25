a5e07090479a1830f303fa7ee61ddd106c6cf5d2
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-08-25 12:38:56 +0100

    respoisnveness fixed

--- src/pages/TrendingMovies.jsx
+++ src/pages/TrendingMovies.jsx
@@ -7,21 +7,25 @@ const TrendingMovies = ({ popularMovies, isLoading, fetchPopularMovies }) => {
 	}, []);
 
 	return (
-		<div className='text-white'>
+		<div className='text-white px-2'>
 			{isLoading && <div>loading...</div>}
 			{popularMovies && (
 				<div>
-					<div className='grid gap-4 grid-cols-5 '>
+					<div className='grid gap-4 col-span-3 lg:grid-cols-5 place-items-center'>
 						{popularMovies?.map((movie, idx) => (
 							<img
 								key={movie?.id}
-								className={idx === 0 ? `col-span-5 row-span-1` : `col-span-1`}
+								className={
+									idx === 0
+										? `col-span-3 lg:col-span-5 row-span-1`
+										: `col-span-1`
+								}
 								src={
 									idx === 0
 										? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
 										: `https://image.tmdb.org/t/p/original/${movie?.poster_path}`
 								}
-								alt={`${popularMovies[0]?.title} poster`}
+								alt={`${movie?.title} poster`}
 							/>
 						))}
 					</div>
