dc7a8583716b364f88e207105fdde7327b071bdf
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-08-25 12:35:39 +0100

    trending movies grid setup

--- src/pages/TrendingMovies.jsx
+++ src/pages/TrendingMovies.jsx
@@ -11,9 +11,20 @@ const TrendingMovies = ({ popularMovies, isLoading, fetchPopularMovies }) => {
 			{isLoading && <div>loading...</div>}
 			{popularMovies && (
 				<div>
-					{popularMovies?.map((movie) => (
-						<div key={movie?.id}>{movie?.title}</div>
-					))}
+					<div className='grid gap-4 grid-cols-5 '>
+						{popularMovies?.map((movie, idx) => (
+							<img
+								key={movie?.id}
+								className={idx === 0 ? `col-span-5 row-span-1` : `col-span-1`}
+								src={
+									idx === 0
+										? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
+										: `https://image.tmdb.org/t/p/original/${movie?.poster_path}`
+								}
+								alt={`${popularMovies[0]?.title} poster`}
+							/>
+						))}
+					</div>
 				</div>
 			)}
 		</div>
