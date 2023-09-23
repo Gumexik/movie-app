fc1df7087545c8a009178734ec53d748ed8cd506
Kacper Gumowski <kacper.gumowski1@gmail.com>
2023-09-23 17:12:26 +0100

    added links and hover effects to featured movies

--- src/pages/TrendingMovies.jsx
+++ src/pages/TrendingMovies.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FadeLoader } from "react-spinners";
 
 /* eslint-disable react/prop-types */
@@ -11,20 +12,42 @@ const TrendingMovies = ({ popularMovies, isLoading }) => {
 			)}
 			{popularMovies && (
 				<div>
-					<div className='grid gap-4 col-span-3 lg:grid-cols-5 place-items-center mt-4'>
+					<div className='grid gap-4 col-span-3 lg:grid-cols-5 place-items-center mt-4 pb-4'>
 						{popularMovies?.map((movie, idx) => (
-							<img
+							<div
 								key={movie?.id}
-								className={`rounded-md
-								${idx === 0 ? `col-span-3 lg:col-span-5 row-span-1` : `col-span-1`}
-								`}
-								src={
-									idx === 0
-										? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
-										: `https://image.tmdb.org/t/p/original/${movie?.poster_path}`
-								}
-								alt={`${movie?.title} poster`}
-							/>
+								className={`rounded-md overflow-hidden relative
+							${idx === 0 ? `col-span-3 lg:col-span-5 row-span-1` : `col-span-1`}
+							`}
+							>
+								<div className='top-0 left-0 h-full w-full hover:bg-black/70 transition-all duration-200 absolute p-4 opacity-0 hover:opacity-100 flex justify-center'>
+									<div>
+										<p className='text-lg font-bold select-none text-center'>
+											{movie.title}
+										</p>
+										<p className='text-sm mt-12 select-none  text-justify'>
+											{movie.overview.length > 150
+												? `${movie.overview.slice(0, 160)}...`
+												: movie.overview}
+										</p>
+									</div>
+									<Link
+										to={`/movies/${movie.id}`}
+										className='text-sm absolute bottom-5 px-4 py-2 bg-yellow-700 rounded-md mx-auto'
+									>
+										Read More
+									</Link>
+								</div>
+								<img
+									loading='lazy'
+									src={
+										idx === 0
+											? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
+											: `https://image.tmdb.org/t/p/original/${movie?.poster_path}`
+									}
+									alt={`${movie?.title} poster`}
+								/>
+							</div>
 						))}
 					</div>
 				</div>
